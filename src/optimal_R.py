7822d54d4537891ac5735cf67b96016df599ff61
JIAQING-XIE <s2001696@ed.ac.uk>
2021-04-11 14:39:43 +0800

    add optimal_R

--- src/optimal_R.py
+++ src/optimal_R.py
@@ -9,22 +9,48 @@ def option():
     return parser.parse_args()
 
 def get_optimal_R():
-    o = option()
     '''
     return the optimal R for the given task and correspomding graph embedding method   
     '''
+    o = option()
     
     graph_embed  = ['SAGE','GAT','GCN','GIN']
     planetoid = ['Cora', 'Citeseer', 'PubMed']
     tudataset = ['ENZYMES','PROTEINS', 'NCI1']
 
+    optimal_R = [[0] * 5 for i in range(0, 5)]
+    optimal_method = [[0] * 5 for i in range(0, 5)]
+
+    if o.dataset in planetoid:
+        path = '/home/jiaqing/桌面/Fea2Fea/Result/Planetoid/' # add your path here
+    elif o.dataset in tudataset:
+        path = '/home/jiaqing/桌面/Fea2Fea/Result/TUdataset/' # add your path here
     for ge in graph_embed:
-        if o.dataset in planetoid:
-            path = '/home/jiaqing/桌面/Fea2Fea/Result/Planetoid/'
-        elif o.dataset in tudataset:
-            path = '/home/jiaqing/桌面/Fea2Fea/Result/TUdataset/'
-        txt = pd.read_csv(path + o.dataset + '_' + ge +'.txt', sep = '\t',header=None) # a matrix
-        print(txt)
+
+        df = pd.read_csv(path + o.dataset + '_' + ge +'.txt', sep = '\t',header=None) # a matrix
+        
+        rows = len(df.axes[0])
+        cols = len(df.axes[1])
+
+        for i in range(0, rows):
+            for j in range(0, cols):
+                if df.iloc[i,j] > optimal_R[i][j]:
+                    optimal_R[i][j] = df.iloc[i,j]
+                    optimal_method[i][j] = ge
+    
+    optimal_R = pd.DataFrame(optimal_R)
+    optimal_method = pd.DataFrame(optimal_method)
+
+    # obtain optimal R and corresponding graph embedding method
+    fig_name1 = o.dataset + '_optimal_R.txt'
+    fig_name2 = o.dataset + '_optimal_method.txt'
+    fig_path1 = path + fig_name1
+    fig_path2 = path + fig_name2
+    optimal_R.to_csv(fig_path1, header = None, index = None, sep = '\t')
+    optimal_method.to_csv(fig_path2, header = None, index = None, sep = '\t')
+                    
+def all_possible_concatenation():
+    
 
 
 if __name__ == '__main__':
