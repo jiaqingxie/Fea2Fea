820b308c2b0ef6eb33093bcefc7cf2c4161fcf71
JIAQING-XIE <s2001696@ed.ac.uk>
2021-06-04 16:52:06 +0800

    add dist

--- src/plot_dist.py
+++ src/plot_dist.py
@@ -9,42 +9,60 @@ txt_dir = os.path.join('/home/jiaqing/桌面/Fea2Fea/', 'Result')
 def read_file(dataset = 'planetoid', dataset_name = 'Cora', Property ='Degree', method = 'dist', count_zero = False):
     dir_name = []
     planetoid = ['PubMed','Cora', 'Citeseer'] # Must in this order
+    tudataset = ['PROTEINS', 'ENZYMES', 'NCI1']
     if dataset == 'planetoid':
         for name in planetoid:
             dir_name.append(txt_dir + '/Planetoid/' + name + '_property.txt')
+    elif dataset == 'tudataset':
+        for name in tudataset:
+            dir_name.append(txt_dir + '/TUdataset/' + name + '_property.txt')
 
     df = pd.DataFrame()
 
-    for (element,name) in zip(dir_name,planetoid):
-        temp = pd.read_csv(element, sep='\t')
-        print(temp.describe())
-        if Property == 'Degree':   
-            prop = temp[temp[Property]<16]
-        if Property == 'Clustering_coefficient':
-            prop = temp[temp[Property]< 0.1]
-        if Property == 'Pagerank':
-            prop = temp[temp[Property]< 0.0008]
-        if Property == 'Aver_path_len':
-            prop = temp[temp[Property]< 16]
-        prop = prop[Property]
-        df[name] = prop
-    df.dropna()
+    if dataset == 'planetoid':
+        for (element,name) in zip(dir_name,planetoid):
+            temp = pd.read_csv(element, sep='\t')
+        #print(temp.describe())
+            if Property == 'Degree':   
+                prop = temp[temp[Property]<16]
+            if Property == 'Clustering_coefficient':
+                prop = temp[temp[Property]< 0.1]
+            if Property == 'Pagerank':
+                prop = temp[temp[Property]< 0.0008]
+            if Property == 'Aver_path_len':
+                prop = temp[temp[Property]< 16]
+            prop = prop[Property]
+            df[name] = prop
+        df.dropna()
+        if count_zero and Property == 'Clustering_coefficient':
+            total_zero = df[dataset_name].value_counts(0)
+            print("\n")
 
-    if count_zero and Property == 'Clustering_coefficient':
-        total_zero = df['Cora'].value_counts(0)
-        print("total nodes with zero clustering coefficient in Cora dataset:{}".format(total_zero))
-        total_zero = df['PubMed'].value_counts(0)
-        print("total nodes with zero clustering coefficient in PubMed dataset:{}".format(total_zero))
-        total_zero = df['Citeseer'].value_counts(0)
-        print("total nodes with zero clustering coefficient in Citeseer dataset:{}".format(total_zero))
-        print("\n")
-        
+    if dataset == 'tudataset':
+        for (element,name) in zip(dir_name,tudataset):
+            temp = pd.read_csv(element, sep='\t')
+        #print(temp.describe())
+            if Property == 'Degree':   
+                prop = temp[temp[Property]<16]
+            if Property == 'Clustering_coefficient':
+                prop = temp[temp[Property]< 2]
+            if Property == 'Pagerank':
+                prop = temp[temp[Property]< 0.2]
+            if Property == 'Aver_path_len':
+                prop = temp[temp[Property]< 16]
+            prop = prop[Property]
+            df[name] = prop
+        df.dropna()
+        if count_zero and Property == 'Clustering_coefficient':
+            total_zero = df[dataset_name].value_counts(0)
+            print("total nodes with zero clustering coefficient in Cora dataset:{}".format(total_zero))
+            print("\n")
 
     if method == 'dist': 
-        plt.figure(figsize=(12,7))
+        plt.figure(figsize=(9,7))
         sns.set(font_scale = 1.5) 
         sns.distplot(df[dataset_name], axlabel = Property)
-        plt.savefig(txt_dir + '/data distribution/planetoid/' + dataset_name +'_' + Property +'_dis.eps', dpi = 800, format = 'eps')
+        plt.savefig(txt_dir + '/data distribution/tudataset/' + dataset_name +'_' + Property +'_dis.eps', dpi = 800, format = 'eps', bbox_inches='tight')
         
         return
     df = df.melt(var_name=Property, value_name='value')
@@ -59,8 +77,9 @@ def read_file(dataset = 'planetoid', dataset_name = 'Cora', Property ='Degree', 
 if __name__ == "__main__":
     prop = ['Degree', 'Clustering_coefficient', 'Pagerank', 'Aver_path_len']
     planetoid = ['PubMed','Cora', 'Citeseer']
-    for dataset_name in planetoid: 
+    tudataset = ['PROTEINS', 'ENZYMES', 'NCI1']
+    for dataset_name in tudataset: 
         for i in prop:
-            read_file(dataset_name = dataset_name, Property=i, count_zero=True)
+            read_file(dataset = 'tudataset', dataset_name = dataset_name, Property=i, count_zero=True)
 
     
